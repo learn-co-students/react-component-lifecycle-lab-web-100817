f08e7fa32308a9ef2e8f897bc6d072af48bbcfa7
Dakota Lillie <dakota.lillie@icloud.com>
2017-12-14 15:49:13 -0500

    Done.

--- src/components/TweetWall.js
+++ src/components/TweetWall.js
@@ -10,9 +10,18 @@ class TweetWall extends React.Component {
     };
   }
 
-  // TODO: componentWillMount()
-  // TODO: shouldComponentUpdate()
-  // TODO: componentWillReceiveProps()
+  componentWillMount = () => {
+    this.setState({ tweets: this.props.newTweets })
+  }
+
+  componentWillReceiveProps = nextProps => {
+    const tweets = nextProps.newTweets.concat(this.state.tweets)
+    this.setState({ tweets: tweets })
+  }
+
+  shouldComponentUpdate = (nextProps, nextState) => {
+    nextState.tweets.length > this.state.tweets.length ? true : false
+  }
 
   render() {
     const tweets = this.state.tweets.map((tweet, index) => <Tweet text={tweet.text} key={index} />);
